2298792a650c59692e10fc481c082e0e2e7bac5a
och-karr <okaa93@gmail.com>
2022-12-07 13:33:17 +0100

    ref: keep selection state by changing list to selection list

--- src/app/components/filtered-product-list-subject/filtered-product-list-subject.component-module.ts
+++ src/app/components/filtered-product-list-subject/filtered-product-list-subject.component-module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
-import { FilteredProductListSubjectComponent } from './filtered-product-list-subject.component';
-import {MatListModule} from "@angular/material/list";
-import {MatCardModule} from "@angular/material/card";
-import {AsyncPipe, NgForOf} from "@angular/common";
-import {RouterLink} from "@angular/router";
+import { CommonModule } from '@angular/common';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
 import { FlexModule } from '@angular/flex-layout/flex';
+import { FilteredProductListSubjectComponent } from './filtered-product-list-subject.component';
+import { AsyncPipe, NgForOf } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @NgModule({
   imports: [
@@ -13,7 +14,8 @@ import { FlexModule } from '@angular/flex-layout/flex';
     AsyncPipe,
     RouterLink,
     NgForOf,
-    FlexModule
+    FlexModule,
+    CommonModule
   ],
   declarations: [FilteredProductListSubjectComponent],
   providers: [],
