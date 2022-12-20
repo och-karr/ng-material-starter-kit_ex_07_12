3aeb66d1aff8d0a3e837addee19e015cdf661abf
och-karr <okaa93@gmail.com>
2022-12-20 19:20:07 +0100

    ref: refactor list with product details next to it - with response mapping

--- src/app/components/product-master-details/product-master-details.component.ts
+++ src/app/components/product-master-details/product-master-details.component.ts
@@ -1,8 +1,7 @@
-import {ChangeDetectionStrategy, Component, ViewEncapsulation} from '@angular/core';
-import {Observable, Subject} from 'rxjs';
-import {switchMap} from 'rxjs/operators';
-import {ProductModel} from '../../models/product.model';
-import {ProductService} from '../../services/product.service';
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { ProductModel } from '../../models/product.model';
+import { ProductService } from '../../services/product.service';
 
 @Component({
   selector: 'app-product-master-details',
@@ -11,17 +10,20 @@ import {ProductService} from '../../services/product.service';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ProductMasterDetailsComponent {
-
   readonly list$: Observable<ProductModel[]> = this._productService.getAll();
-  private _selectedProductIdSubject: Subject<number> = new Subject<number>();
-  public selectedProductId$: Observable<number> = this._selectedProductIdSubject.asObservable();
-  readonly details$: Observable<ProductModel> = this.selectedProductId$.pipe(
-    switchMap(data => this._productService.getOne(data)));
+  private _detailsSubject: Subject<ProductModel> = new Subject<ProductModel>();
+  public details$: Observable<ProductModel> = this._detailsSubject.asObservable();
+  // readonly list$: Observable<ProductModel[]> = this._productService.getAll();
+  // private _selectedProductIdSubject: Subject<number> = new Subject<number>();
+  // public selectedProductId$: Observable<number> = this._selectedProductIdSubject.asObservable();
+  // readonly details$: Observable<ProductModel> = this.selectedProductId$.pipe(
+  //   switchMap(data => this._productService.getOne(data)));
 
   constructor(private _productService: ProductService) {
   }
 
-  selectProduct(id: number): void {
-    this._selectedProductIdSubject.next(id);
+  selectProduct(product: ProductModel): void {
+    // this._selectedProductIdSubject.next(id);
+    this._detailsSubject.next(product);
   }
 }
